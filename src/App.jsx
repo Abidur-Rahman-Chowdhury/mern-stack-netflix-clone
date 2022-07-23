2e1e5bd5c01643876de0f86f62d8fb70b31f8b5f
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-07-23 21:34:05 +0600

    complete sign up component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Netflix from './pages/Netflix'
-import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 function App() {
   return (
     
       <Routes>
-        <Route path="/" element={<Netflix />} />
-        <Route path='/login' element={<Login></Login>}></Route>
+        <Route  path="/" element={<Netflix />} />
+        <Route  path='/login' element={<Login />}></Route>
+        <Route  path='/signup' element={<Signup />}></Route>
       </Routes>
     
   )
